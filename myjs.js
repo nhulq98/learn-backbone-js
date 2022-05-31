b03255ffb1933069e84c22dde5a00fc92b82ea96
nhulq98 <nhule2031998@gmail.com>
2022-06-01 05:43:28 +0700

    add backbone internal library

--- myjs.js
+++ myjs.js
@@ -1,8 +1,53 @@
+var h1Node = document.querySelector('h1');
+
+// var assignEvent = (item)=>{
+//     // item.onclick = (e)=>{console.log(e.target)};
+//     item.onclick = function(this){console.log(this)};
+// }
+
+// h1Node.forEach(item=>{
+//     // item.onclick = (e) => {console.log(this)};
+//     item.addEventListener('click', function(){console.log(this)});
+// });
+
+
+// create custom events
+const catFound = new CustomEvent('animalfound', {
+    detail: {
+      name: 'cat'
+    }
+  });
+  const dogFound = new CustomEvent('animalfound', {
+    detail: {
+      name: 'dog'
+    }
+  });
+
+  // add an appropriate event listener
+  h1Node.addEventListener('animalfound', (e) => console.log(e.detail.name));
+
+
+  h1Node.dispatchEvent(catFound);
+  h1Node.dispatchEvent(dogFound);
+
+
+window.onload = () =>{// it is fired when the entire page loads
+    console.log('window.onload');
+}
+window.onunload = () =>{// occurs when user unload page
+    console.log('window.onunload');
+}
+document.onload = () =>{// occurs when user unload page
+    console.log('document.onload');
+}
+
+var ok = Backbone.Model.extend();
+
 //======= reder data to view scope============
 first_view = Backbone.View.extend({
     el: 'body',
     initialize: function () {
-        this.$el.empty();// reset view
+        // this.$el.empty();// reset view
         this.render();
     },
 
@@ -16,6 +61,9 @@ first_view = Backbone.View.extend({
 
 List_view = Backbone.View.extend({
     tagName: 'ul',
+    id: 'listDogs',
+    className: 'list-dog',
+    // el: 'ul',
     render: function () {
         this.$el.empty();
         this.$el.append("<li>Nhu</li>");
@@ -25,3 +73,20 @@ List_view = Backbone.View.extend({
     }
 });
 index_view = new first_view();
+
+// var Sidebar = Backbone.Model.extend({
+//     promptContent: function() {
+//       var content = prompt("Please enter a CSS color:");
+//       this.set({text: content});
+//     }
+//   });
+  
+//   window.sidebar = new Sidebar();
+  
+//   sidebar.on('change:text', function(model, text) {
+//     $('#listDogs').css({text: text});
+//   });
+  
+//   sidebar.set({text: 'white'});
+  
+//   sidebar.promptContent();
